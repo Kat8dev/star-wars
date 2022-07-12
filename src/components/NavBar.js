8f870a879e57014d55357883b9db6c42856dce80
Khatuna jabakhidzd <MY_NAME@example.com>
2022-07-12 11:59:07 +0200

    add some styles

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,18 +4,25 @@ import styled from "styled-components";
 const NavBar = () => {
     return (
         <NAV_CONTAINER>
-            <UpperBar>
+            <UPPER_BAR_WRAPPER>
+                <EXTERNAL_LINKS>
                 
-            </UpperBar>
-            <LinkBar>
+                  {/*<Link to={{pathname: "https://www.facebook.com/StarWars" }} target="_blank" > <span>algo</span> </Link>*/}
+                  <Link to={{pathname:`${"https://www.facebook.com/StarWars"}`}}>View</Link>
+              
+                </EXTERNAL_LINKS>
+                <LOGO />
+            </UPPER_BAR_WRAPPER>
+            <LINK_BAR>
                 <Link className="APP_LINKS" to="/">Welcome</Link>
                 <Link className="APP_LINKS" to="/ShipList">Ship List</Link>
-            </LinkBar>
+            </LINK_BAR>
         </NAV_CONTAINER>
     )
 }
 
 export default NavBar;
+
 
 export const NAV_CONTAINER = styled.div`
     width: 100%;
@@ -23,7 +30,7 @@ export const NAV_CONTAINER = styled.div`
     top: 0;
 `
 
-export const LinkBar = styled.nav`
+export const LINK_BAR = styled.nav`
     left: 0;
     width: 100%;
     height: auto;
@@ -37,12 +44,29 @@ export const LinkBar = styled.nav`
     border-top: 1px solid #343434;
 `
 
-export const UpperBar = styled.div`
+export const UPPER_BAR_WRAPPER = styled.div`
     max-width: 1400px;
     max-height: 134px;
     margin: 0 auto;
     padding: 20px 0;
     box-sizing: border-box;
-    background-image: url(images/star-wars-logo.jpg);
+    
 `
 
+export const EXTERNAL_LINKS = styled.div`
+    width: 400px;
+`
+
+export const LOGO = styled.div`
+    width: 100%;
+    max-width: 203px; 
+    height: 6rem;
+    margin: 6px auto 0;
+    background-image: url(https://static-mh.content.disney.io/starwars/assets/navigation/sw_logo_stacked-336c62367939.png);
+    background-size: contain;
+    background-repeat: no-repeat;
+`
+
+export const LOG_IN = styled.div`
+    width: 200px;
+`
